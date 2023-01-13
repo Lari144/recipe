57de4426d6cf64469f0954a7402dc32bdb5b7654
Lari144 <55692422+Lari144@users.noreply.github.com>
2023-01-13 23:38:35 +0100

    added search option

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class EntryWindow:
         self.quit_button = Button(self.master, text = 'Quit', width = 25, command = lambda:self.close_windows(oldmaster))
         headline_ingredients = Label(self.master, text="Enter your Ingredients")
         headline_name = Label(self.master, text="Enter your Recipes name")
-        self.submit_button = Button(self.master, text='Submit', command=lambda:self.store_data())
+        self.submit_button = Button(self.master, text='Submit', command=lambda:RecipeDatabase.store_data(self))
         self.entry_name = Entry(self.master)
         self.entry_ingredients = Entry(self.master)
         
@@ -48,6 +48,29 @@ class EntryWindow:
         entry = self.entry_ingredients.get()
         print(f"{name}\n{entry}")
     
+
+class SearchWindow:
+    def __init__(self, master, oldmaster):
+        self.master = master
+        self.headline = Label(self.master, text = 'Search for your Recipe here')
+        self.quit_button = Button(self.master, text = 'Quit', width = 25, command = lambda:self.close_windows(oldmaster))
+        self.search_button = Button(self.master, text='Submit', command=lambda:RecipeDatabase.search_data(self))
+        self.entry_name = Entry(self.master)
+        
+        self.headline.pack()
+        self.entry_name.pack()
+        self.search_button.pack()
+        self.quit_button.pack()
+
+    def close_windows(self, oldmaster):
+        oldmaster.deiconify()
+        self.master.destroy()
+
+class RecipeDatabase(EntryWindow):
+    
+    def __init__(self, master, oldmaster):
+        super().__init__(master, oldmaster)
+        
     def store_data(self):
         name = self.entry_name.get()
         ingredients = self.entry_ingredients.get()
@@ -66,28 +89,14 @@ class EntryWindow:
         cursor.execute(data_insert_query, data_insert_tuple)
         conn.commit()
         conn.close()
-
-class SearchWindow:
-    def __init__(self, master, oldmaster):
-        self.master = master
-        self.headline = Label(self.master, text = 'Search for your Recipe here')
-        self.quit_button = Button(self.master, text = 'Quit', width = 25, command = lambda:self.close_windows(oldmaster))
-        self.submit_button = Button(self.master, text='Submit', command=lambda:self.get_input())
-        self.text = Text(self.master, width=80, height=15)
-        
-        self.headline.pack()
-        self.submit_button.pack()
-        self.text.pack()
-        self.quit_button.pack()
-
-    def close_windows(self, oldmaster):
-        oldmaster.deiconify()
-        self.master.destroy()
     
-    def get_input(self):
-        input = self.entry_ingredients.get()
-        print(input)
-
+    def search_data(self):
+        conn = sqlite3.connect('recipe.db')
+        cursor = conn.execute('SELECT * FROM Recipe_Data WHERE Name LIKE ?', ('%' + str(self.entry_name.get()),))
+        print(cursor.fetchall())
+    
+    def display_searched_data(self):
+        pass
 
 def main(): 
     root = Tk()
